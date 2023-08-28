e9fa14029a9f197c83063909d55baefd64ab5d8a
Emmanuelmshindi <mshindimanuel@gmail.com>
2023-08-28 14:50:43 -0700

    use puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,13 +2,13 @@
 # Edit configuration file using puppet
 
 file { 'etc/ssh/ssh_config':
-         ensure => present,
+        ensure => present,
 
 content =>" 
 
         #SSH client configuration
-	host*
-	IdentityFile /alx-system_engineering-devops/0x0B-ssh/school
+	Host *
+	IdentityFile ~/.ssh/school
 	PasswordAuthentication no
 	",
 }
