40459158407161442bb3698a6850c8222a3bbef5
Emmanuelmshindi <mshindimanuel@gmail.com>
2023-08-29 14:58:26 -0700

    Task 1

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,15 +1,9 @@
-/* 
-   Create a file in the path /tmp/school
-   File permission is 0744, owner and group is www-data
-   File contains I love Puppet
-*/
+# Create a file named school inside the tmp folder
 
-Node default{
 file{ '/tmp/school':
   ensure     => file,
   owner      => 'www-data',
-  group      => 'www-data'
+  group      => 'www-data',
   mode       => '0744',
   content    => 'I love Puppet',
-}
 }
