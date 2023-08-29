83664b129ed897bdd6663abce07fb9f74af0839e
Emmanuelmshindi <mshindimanuel@gmail.com>
2023-08-29 15:05:03 -0700

    Task 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,6 @@
-/* Manifest to install a package 
-   Install flask version 2.1.0 from pip3
-*/
+# Manifest to install a package
 
-class flask_installer{
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-}
 }
