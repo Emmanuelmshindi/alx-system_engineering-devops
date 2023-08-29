00ea82db0e028e08e759ca29bd0931e3fc125b4a
Emmanuelmshindi <mshindimanuel@gmail.com>
2023-08-29 15:27:29 -0700

    Task2

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 # Create a file named school inside the tmp folder
-
-file{ '/tmp/school':
-  ensure     => file,
-  owner      => 'www-data',
-  group      => 'www-data',
-  mode       => '0744',
-  content    => 'I love Puppet',
+file { '/tmp/school':
+  ensure   => file,
+  owner    => 'www-data',
+  group    => 'www-data',
+  mode     => '0744',
+  content  => 'I love Puppet',
 }
