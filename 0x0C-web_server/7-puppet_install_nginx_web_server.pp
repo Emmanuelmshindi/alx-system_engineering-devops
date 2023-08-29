437ade56b0ea97864ac47bb9457c7346b14350f5
Emmanuelmshindi <mshindimanuel@gmail.com>
2023-08-29 14:10:40 -0700

    Puppet configuration

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -5,13 +5,14 @@ package { 'nginx':
 }
 file_line { 'install':
   ensure => 'present',
-  path   => 'etc/nginx/sites-enabled/default',
+  path   => '/etc/nginx/sites-enabled/default',
   after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://www.github.com/Emmanuelmshindi permanent;',
 }
 file { '/var/www/html/index.html':
   content => 'Hello World!',
 }
+
 service { 'nginx':
   ensure => running,
   require => Package['nginx'],
