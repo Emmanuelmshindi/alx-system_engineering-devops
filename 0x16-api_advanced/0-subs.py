e72f8a35977ceee399ca833741f2109e5ae8bb82
Emmanuelmshindi <mshindimanuel@gmail.com>
2023-10-18 22:58:42 -0700

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 
+"""
+Reddit API Subreddit Subscriber Count
+This module defines a function to query the Reddit API and 
+return the number of subscribers for a given subreddit.
+
+Requirements:
+- No authentication is necessary for most features of the Reddit API.
+- If not a valid subreddit, the function returns 0.
+- Invalid subreddits may return a redirect to search results.
+Ensure that you are not following redirects.
+"""
+
 import requests
 
 def number_of_subscribers(subreddit):
+    """
+    Retrieves number of subscribers from a given subreddit
+    Args:
+        subnreddit(str): subreddit to query
+    Return:
+        Number of subscribers or 0 if subreddit is invalid
+    """
     # Define API endpoint url
     api_url = "https://www.reddit.com/r/{subreddit}/about.json"
 
